4189263b96aa9ea0775aec239da6f3ac8d9141b0
OD-Oraf <43830269+OD-Oraf@users.noreply.github.com>
2020-12-25 18:44:36 -0500

    dec25 18:44
    still need to fix images in app.css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.css';
 import Navbar from './components/Navbar.js';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
+
 
 function App() {
   return (
@@ -12,7 +16,13 @@ function App() {
       <Router>
         <Navbar />
         <Switch> 
+          
           <Route path = '/' exact component=  {Home}/>
+          <Route path = '/services' component=  {Services}/>
+          <Route path = '/products' component=  {Products}/>
+          <Route path = '/sign-up' component=  {SignUp}/>
+          
+
         </Switch>
         </Router>
   
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+ import React from 'react'
 import '../../App.css';
 import Hero from '../Hero';
 import Cards from '../Cards';
